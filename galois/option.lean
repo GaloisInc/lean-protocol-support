6e5b88c5e49a9fa3c7bafd935daba0747036f2a8
Joe Hendrix <jhendrix@galois.com>
2017-06-06 10:43:42 -0700

    Additional simp rules for option.

--- galois/option.lean
+++ galois/option.lean
@@ -1,10 +1,35 @@
 /- This module defines lemmas for option -/
 
-universe variable u
+universe variables u v
 
 namespace option
 
+variables {α : Type u} {β : Type u}
+
 @[simp]
-theorem is_some_none {α : Type u} : is_some (none : option α) = ff := rfl
+theorem is_some_none  : is_some (none : option α) = ff := rfl
+
+@[simp]
+theorem has_map_none (f : α → β) : f <$> none = none := rfl
+
+@[simp]
+theorem has_map_some (f : α → β) (x : α) : f <$> some x = some (f x) := rfl
+
+@[simp]
+theorem none_orelse (x : option α) : (none <|> x) = x :=
+begin
+  cases x,
+  all_goals { refl },
+end
+
+@[simp]
+theorem some_orelse (x : α) (y : option α) : (some x <|> y) = some x := rfl
+
+@[simp]
+theorem orelse_none (x : option α) : (x <|> none) = x :=
+begin
+  cases x,
+  all_goals { refl },
+end
 
 end option
