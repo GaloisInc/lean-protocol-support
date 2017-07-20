6c8f5ff5b2ba7579279f5156df371a70b03e1c28
Ben Sherman <bsherman@galois.com>
2017-07-20 15:52:27 -0700

    Travis CI: enabled caching of Lean build

--- .travis/install-lean.sh
+++ .travis/install-lean.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/leanprover/lean
+git clone https://github.com/leanprover/lean || true
 cd lean/
 git checkout ae5bc52
-mkdir build
+mkdir -p build
 cd build
 cmake -DCMAKE_CXX_COMPILER=g++-6 ../src
 make -j 2
