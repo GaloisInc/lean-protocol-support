470da6d842f1082d3e2118170bde749e04313a0b
Ben Sherman <bsherman@galois.com>
2017-08-04 13:56:49 -0700

    Add apply_in tactic

--- galois/tactic.lean
+++ galois/tactic.lean
@@ -30,8 +30,26 @@ def congr_arg_f_equal {A : Sort u} {B : Sort v} {f f' : A → B}
 intros H, induction H, intros H, induction H, reflexivity
 end
 
-meta def f_equal : tactic unit := do
-  try (apply ``(@congr_arg_f_equal)),
-  tactic.focus [ reflexivity <|> f_equal, try reflexivity ]
+meta def f_equal : tactic unit :=
+  try (do apply ``(@congr_arg_f_equal),
+  tactic.focus [ reflexivity <|> f_equal, try reflexivity ])
+
+meta def apply_in_aux : expr → tactic expr
+| (pi x info ty e) := do
+     x' ← mk_meta_var ty,
+     apply_in_aux (e.instantiate_var x')
+| e := pure e
+
+meta def apply_in (H : parse ident) (e : parse texpr) 
+  : tactic unit := do
+  e' ← i_to_expr e,
+  ty_e ← infer_type e' >>= whnf,
+  goal ← apply_in_aux ty_e,
+  n ← get_unused_name H,
+  tactic.assert n goal,
+  tactic.apply e',
+  He ← get_local H,
+  any_goals (tactic.apply He),
+  any_goals (do clear [H], rename n H)
 
 end tactic.interactive
