29957c520d79ffa310368044f6c04d34e809e4bb
Joey Dodds <jdodds@galois.com>
2017-08-08 16:52:30 -0600

    fix build

--- galois/data/tree.lean
+++ galois/data/tree.lean
@@ -1,4 +1,4 @@
-import aim.list
+import galois.list
 
 namespace tree
 
@@ -59,53 +59,6 @@ def update_index_item {a : Type} (i: a) : list ℕ -> tree a -> option (tree a)
 | (h::t) (tree.node i cs) := do nc <- list.nth cs h,
                               uc <- update_index_item t nc,
                               return (tree.node i (list.update_nth cs h uc))
-
--- if we get a node by index, dropping the last step of that index gives us the parent of that node
-lemma dropLastHasParent : forall {a: Type} (i : list nat) (t : tree a) (p: a) (c:a),
-i ≠ [] ->
-some c = at_index i t ->
-some p = at_index (list.drop_last i) t ->
-hasparent p c t :=
-sorry /-begin
-intros _ _ _ _ _ lne sc cp, cases t with it cs,
-induction i with h t ih,
-  contradiction,
-  simp [at_index] at sc,
-  unfold list.drop_last at cp,
-  cases (list.nth cs h) with c,
-    unfold bind at sc,
-end
--/
-
-lemma bind_some : forall {a β: Type} (i : a) (f : a → option β),
-(some i >>= f) = f i :=
-begin
-  admit
-end
-
-
-lemma atIndex_update : forall {a : Type} (t nt: tree a) (i : list nat) (n : a),
-    update_index_item n i t = some nt ->
-    at_index i nt = some n :=
-begin
-intros,
-induction i,
- { cases t,
-    simp [update_index_item] at a_1,
-    injection a_1,
-    subst nt,
-    simp [at_index, subtree_at_index, bind_some, rootitem],
-    trivial,
- },
- {
-    rename a_1 a,
-     cases t,
-     simp [update_index_item] at a,
-     admit,
-
- }
-
-end
 
 
 end tree
