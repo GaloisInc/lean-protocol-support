c2bb3204a72e58da45c2977a89f703c8f1c882fd
Ben Sherman <bsherman@galois.com>
2017-08-29 14:28:52 -0700

    Generalize binary tree paths functions to arbitrary link types

--- galois/data/binary_tree2.lean
+++ galois/data/binary_tree2.lean
@@ -3,9 +3,8 @@
 -- https://arxiv.org/pdf/1401.7886.pdf
 -- author: Ben Sherman
 import data.list.basic
-
-import galois.list.preds
-import galois.tactic
+       galois.list.preds
+       galois.tactic
 
 namespace binary_tree
 
@@ -814,52 +813,59 @@ inductive lr : Type
 @[reducible]
 def path := list (lr × A)
 
-def all_paths_core : tree A -> path -> list (A × path)
+
+parameter {link : Type}
+parameter mk_link : lr → A → A → link
+
+def all_paths_core : tree A -> list link -> list (list link)
 | (tree.node i l r) path :=
-                 all_paths_core r ((lr.right, root l) ::path)
-              ++ all_paths_core l ((lr.left, root r) ::path)
-| (tree.leaf i) path := [(i, path)]
+                 all_paths_core r ((mk_link lr.left (root l) (root r)) ::path)
+              ++ all_paths_core l ((mk_link lr.right (root l) (root r)) ::path)
+| (tree.leaf i) path := [path]
 
 /-- get all paths (in order left to right) from root to leaf
     these paths start with leafs, and are meant to be followed
     up the tree in order to reconstruct a root in log(n) operations
  -/
-def all_paths (tr : tree A): list (A × path) :=
+def all_paths (tr : tree A): list (list link) :=
               all_paths_core tr []
 
 def second {X A B : Type} (f : A -> B) : X × A -> X × B
 | (x, y) := (x, f y)
 
-def all_paths' : tree A -> list (A × path)
-| (tree.leaf i) := [(i, [])]
+def all_paths' : tree A -> list (list link)
+| (tree.leaf i) := [[]]
 | (tree.node i l r) :=
-     list.map (second (λ p, p ++ [(lr.right, root l)])) (all_paths' r)
-  ++ list.map (second (λ p, p ++ [(lr.left, root r)])) (all_paths' l)
+     list.map (λ p, p ++ [mk_link lr.left (root l) (root r)]) (all_paths' r)
+  ++ list.map (λ p, p ++ [mk_link lr.right (root l) (root r)]) (all_paths' l)
 
 /-- Get all paths, but this time the paths give the direction
     to follow from the root to the leaf. Finally, we also return the
     leaf element that the path leads to.
 -/
-def all_paths_rev : tree A -> list (A × path)
-| (tree.leaf i) := [(i, [])]
-| (tree.node i l r) :=
-     list.map (second (list.cons (lr.right, root l))) (all_paths_rev r)
-  ++ list.map (second (list.cons (lr.left , root r))) (all_paths_rev l)
 
-lemma all_paths_core_app (t : tree A) (p p' : path)
+def all_paths_rev : tree A -> list (list link)
+| (tree.leaf i) := [[]]
+| (tree.node i l r) :=
+     list.map (list.cons (mk_link lr.left (root l) (root r))) (all_paths_rev r)
+  ++ list.map (list.cons (mk_link lr.right (root l) (root r))) (all_paths_rev l)
+
+lemma all_paths_core_app (t : tree A) (p p' : list link)
   : all_paths_core t (p ++ p') =
-    list.map (second (λ z, z ++ p')) (all_paths_core t p)
+    list.map (λ z, z ++ p') (all_paths_core t p)
 := begin
 revert p p',
 induction t; intros,
 { reflexivity },
-{ simp [all_paths_core],
-  rw [<-list.cons_append, <-list.cons_append],
-  rw [ih_1, ih_2] }
+{ dsimp [all_paths_core],
+  repeat { rw ← list.cons_append },
+  rw [ih_1, ih_2],
+  rw list.map_append,
+}
 end
 
-lemma all_paths_same_core (t : tree A) (p : path)
-  : all_paths_core t p = list.map (second (λ z, z ++ p)) (all_paths' t)
+lemma all_paths_same_core (t : tree A) (p : list link)
+  : all_paths_core t p = list.map (λ z, z ++ p) (all_paths' t)
 :=
 begin
 revert p,
@@ -869,13 +875,16 @@ induction t; intros,
   f_equal,
   { rw ih_2,
     f_equal, apply funext,
-    intros x, cases x with i1 i2,
-    dsimp [function.comp, second],
-    simp [second] },
+    intros x,
+    dsimp [function.comp],
+    rw list.append_assoc,
+    reflexivity, },
   { rw ih_1,
     f_equal, apply funext,
-    intros, cases x with i1 i2,
-    dsimp [function.comp, second], simp [second] }
+    intros x,
+    dsimp [function.comp],
+    rw list.append_assoc,
+    reflexivity, }
 }
 end
 
@@ -885,15 +894,14 @@ lemma all_paths_same (t : tree A)
 unfold all_paths,
 rw all_paths_same_core,
 apply list.map_id',
-intros x, cases x with i1 i2,
-simp [second]
+intros x, simp,
 end
 
 /-- Indeed, `all_paths` and `all_paths` are the same, except one
     gives paths in the reverse order compared to the other
 -/
 lemma all_paths_reverse (t : tree A)
-  : list.map (second list.reverse) (all_paths_rev t) = all_paths' t
+  : list.map (list.reverse) (all_paths_rev t) = all_paths' t
 :=
 begin
 induction t,
@@ -903,13 +911,13 @@ induction t,
   { rw <- ih_2,
     rw <- list.map_compose,
     f_equal, apply funext,
-    intros x, cases x with lf xs,
-    simp [function.comp, second] },
+    intros x,
+    simp [function.comp], },
   { rw <- ih_1,
     rw <- list.map_compose,
     f_equal, apply funext,
-    intros x, cases x with lf xs,
-    simp [function.comp, second]  }
+    intros x,
+    simp [function.comp]  }
 }
 end
 
@@ -933,15 +941,14 @@ def tree_at_path : tree A -> path -> option (tree A)
                                 end
 | (tree.leaf _) _ := none
 
+parameter (compute_parent : link → A → A)
 
-def reconstruct_root (f : A -> A -> A) : path -> A -> A
-| [] lf := lf
-| ((lr.left, i) :: xs) lf := f (reconstruct_root xs lf) i
-| ((lr.right, i) :: xs) lf := f i (reconstruct_root xs lf)
+def reconstruct_root (xs : list link) (x : A) : A
+  := xs.foldr compute_parent x
 
-def reconstruct_items_rev (f : A → A → A) : path → A → list A
+def reconstruct_items_rev : list link → A → list A
 | [] lf := []
-| (x :: xs) lf := reconstruct_root f (x :: xs) lf :: reconstruct_items_rev xs lf
+| (x :: xs) lf := reconstruct_root (x :: xs) lf :: reconstruct_items_rev xs lf
 
 /-- Rebuilds all of the items in a path given the combination function, a
     path, and an initial value, path moves towards the root-/
@@ -971,26 +978,104 @@ lemma second_simpl {X A B : Type} (f : A -> B)
   : second f (x, y) = (x, f y)
   := rfl
 
+parameter combine_data : A → A → A
+def compute_parent_correct := forall (left right : A) dir,
+  compute_parent (mk_link dir left right) (match dir with
+                  | lr.left := right
+                  | lr.right := left end)
+    = combine_data left right
+
+lemma tree_leaves_all_paths_rev_len (x : tree A)
+  : (tree_leaves x).length = (all_paths_rev x).length
+:= begin
+induction x; dsimp [tree_leaves, all_paths_rev, list.length],
+{ reflexivity },
+{ repeat { rw list.length_append },
+  repeat { rw list.length_map },
+  f_equal; assumption,
+}
+end
+
+
+
+lemma list.pair_induction_same_length {X Y}
+  (P : list X → list Y → Sort u)
+  (P0 : P [] [])
+  (PS : ∀ x y xs ys, P xs ys → P (x :: xs) (y :: ys))
+  (xs : list X) (ys : list Y) (H : xs.length = ys.length)
+  : P xs ys
+:= begin
+revert xs, induction ys; intros; dsimp at H,
+{ apply_in H list.eq_nil_of_length_eq_zero, subst xs,
+  assumption, },
+{ cases xs; dsimp at H, contradiction,
+  apply PS, apply ih_1,
+  apply nat.add_right_cancel, assumption,
+}
+end
+
+lemma list.zip_same_length {X Y}
+  (xs ys : list X) (xs' ys' : list Y) (H : xs.length = xs'.length)
+  : list.zip (xs ++ ys) (xs' ++ ys')
+  = list.zip xs xs' ++ list.zip ys ys'
+:= begin
+revert H xs' xs,
+apply list.pair_induction_same_length,
+reflexivity, intros, dsimp [list.zip, list.zip_with],
+f_equal, assumption
+end
+
+lemma list.zip_map_r {X Y Z} (xs : list X) (ys : list Y)
+  (f : Y → Z)
+  : list.zip xs (list.map f ys) =
+  list.map (second f) (list.zip xs ys)
+:= begin
+revert ys, induction xs; intros,
+{ reflexivity },
+{ cases ys,
+  { reflexivity },
+  { dsimp [list.zip, list.zip_with], f_equal,
+    specialize (ih_1 a_3),
+    unfold list.zip at ih_1,
+    rw ih_1,
+  }
+ }
+end
+
+lemma tree_leaves_all_paths_rev {X : Type u} (l r : tree A) (f g : list link → X) :
+  (list.zip (tree_leaves r ++ tree_leaves l)
+       (list.map f (all_paths_rev r) ++ list.map g (all_paths_rev l)))
+  =
+  list.zip (tree_leaves r) (list.map f (all_paths_rev r))
+  ++ list.zip (tree_leaves l) (list.map g (all_paths_rev l))
+:= begin
+apply list.zip_same_length, rw list.length_map,
+apply tree_leaves_all_paths_rev_len,
+end
+
+
 /-- If we compute the list of paths to all leaves in a tree
     where the internal nodes are all the result of applying
     and then use those paths and the leaves they point to
     to try to
 
 -/
-lemma reconstruct_root_correct (f : A -> A -> A)
+lemma reconstruct_root_correct
   (t : tree A)
-  (H : internal_nodes_ok f t)
-  : list.Forall (λ p : A × path,
-    reconstruct_root f p.snd p.fst = root t) (all_paths_rev t)
+  (H : internal_nodes_ok combine_data t)
+  (comp_par_corr : compute_parent_correct)
+  : list.Forall (λ p : A × list link,
+    reconstruct_root p.snd p.fst = root t) (list.zip (tree_leaves t) (all_paths_rev t))
 := begin
 induction H,
 { simp [all_paths_rev], constructor,
   { dsimp, simp [reconstruct_root], reflexivity },
   { constructor }
 },
-{ simp [all_paths_rev],
+{ dsimp [all_paths_rev, tree_leaves],
+  rw tree_leaves_all_paths_rev,
   apply list.concat_Forall,
-  { clear ih_1, apply list.map_Forall,
+  { clear ih_1, rw list.zip_map_r, apply list.map_Forall,
     apply list.impl_Forall, apply ih_2,
     clear ih_2 a a_1,
     simp [root],
@@ -998,9 +1083,12 @@ induction H,
     subst x,
     intros H,
     dsimp [second],
-    simp [reconstruct_root],
-    rw <- H },
-  { clear ih_2, apply list.map_Forall,
+    dsimp [reconstruct_root],
+    have H' := comp_par_corr (root l) (root r) lr.left,
+    simp [compute_parent_correct] at H',
+    dsimp [reconstruct_root] at H,
+    rw H, apply H', },
+  { clear ih_2, rw list.zip_map_r, apply list.map_Forall,
     apply list.impl_Forall, apply ih_1,
     clear ih_1 a a_1,
     simp [root],
@@ -1009,7 +1097,10 @@ induction H,
     intros H,
     simp [second],
     simp [reconstruct_root],
-    rw <-H  }
+    have H' := comp_par_corr (root l) (root r) lr.right,
+    simp [compute_parent_correct] at H',
+    dsimp [reconstruct_root] at H,
+    rw H, apply H', }
 }
 end
 
@@ -1027,52 +1118,47 @@ induction xs,
 }
 end
 
-/-- The leaves produced by the `all_paths_rev`
-    function agree with the leaves produced
-    by `tree_leaves`
--/
-lemma all_paths_rev_tree_leaves (f : A -> A -> A)
+lemma reconstruct_items_correct
+  (comp_par_corr : compute_parent_correct)
   (t : tree A)
-  : list.map prod.fst (all_paths_rev t)
-  = tree_leaves t
-:= begin
-induction t,
-{ reflexivity },
-{ simp [tree_leaves, all_paths_rev],
-  rw list.map_fst_second,
-  rw list.map_fst_second,
-  rw ih_1, rw ih_2
-  }
-end
-
-lemma reconstruct_items_correct (f : A -> A -> A)
-  (t : tree A)
-  : internal_nodes_ok f t
-  -> list.Forall (λ p, let (lf, pth) := p in
-      path_in_tree_rev lf (reconstruct_items_rev f pth lf) t) (all_paths_rev t)
+  : internal_nodes_ok combine_data t
+  -> list.Forall (λ p : A × list link, let (lf, pth) := p in
+      path_in_tree_rev lf (reconstruct_items_rev pth lf) t)
+        (list.zip (tree_leaves t) (all_paths_rev t))
 := begin
 intros H, induction H,
-{ simp [all_paths_rev],
+{ dsimp [all_paths_rev],
   constructor, dsimp, constructor, constructor },
 { dsimp at ih_1 ih_2,
-  dsimp, simp [all_paths_rev],
+  dsimp, dsimp [all_paths_rev, tree_leaves],
+  rw tree_leaves_all_paths_rev,
   apply list.concat_Forall,
-  { apply list.map_Forall,
+  { rw list.zip_map_r, apply list.map_Forall,
     apply_in a_1 (reconstruct_root_correct),
+    apply comp_par_corr,
     apply list.impl_Forall2, apply a_1, apply ih_2,
     intros p H H', induction p with lf pth,
     dsimp at H H', dsimp [second],
-    simp [reconstruct_items_rev],
-    simp [reconstruct_root], rw H,
+    dsimp [reconstruct_items_rev],
+    dsimp [reconstruct_root],
+    dsimp [reconstruct_root] at H, rw H,
+    have H2 := comp_par_corr (root l) (root r) lr.left,
+    dsimp [compute_parent_correct] at H2,
+    rw H2,
     rw ← a_2, apply path_in_tree_rev.right, assumption,
   },
-  { apply list.map_Forall,
+  { rw list.zip_map_r, apply list.map_Forall,
     apply_in a reconstruct_root_correct,
+    apply comp_par_corr,
     apply list.impl_Forall2, apply a, apply ih_1,
     intros p H H', induction p with lf pth,
     dsimp at H H', dsimp [second],
     simp [reconstruct_items_rev],
-    simp [reconstruct_root], rw H,
+    simp [reconstruct_root],
+    dsimp [reconstruct_root] at H, rw H,
+    have H2 := comp_par_corr (root l) (root r) lr.right,
+    dsimp [compute_parent_correct] at H2,
+    rw H2,
     rw ← a_2, apply path_in_tree_rev.left, assumption, }
 }
 end
