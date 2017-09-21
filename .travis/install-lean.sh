31a3a2ef12de7726c89ec64ff054f68c21faa4ad
Joey Dodds <jdodds@galois.com>
2017-09-21 14:47:57 -0700

    bump lean version

--- .travis/install-lean.sh
+++ .travis/install-lean.sh
@@ -1,7 +1,7 @@
 cd ../
 git clone https://github.com/leanprover/lean || true
 cd lean/
-git checkout fa9c868ed2bba1b6e38c49c03a75e86c6cbda0ac
+git checkout d83b9ef3efbf
 mkdir -p build
 cd build
 cmake -DCMAKE_CXX_COMPILER=g++-6 ../src
